d9874e183263014af7df75517fcedfdbd64c2a8f
Richard Zozimo <richardzozimo@gmail.com>
2022-08-06 18:29:52 -0300

    criando endpoints de acesso

---
+++ Controllers/ObjetivoFinanceiroController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PoupaDev.API.Models;
+
+namespace PoupaDev.API.Controllers
+{
+    [ApiController]
+    [Route("api/ojetivos-financeiros")]
+    public class ObjetivoFinanceiroController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTodos() {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPorId(int id) {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(ObjetivoFinanceiroInputModel model)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}/operacoes")]
+        public IActionResult PostOperacao(int id, ObjetivoFinanceiroInputModel model)
+        {
+            return Ok();
+        }
+    }
+}
